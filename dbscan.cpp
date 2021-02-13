948613a75b15ce247eb30d144ea92349f16261de
Artyom1363 <artemwulf2015@gmail.com>
2021-02-13 22:31:13 +0300

    dbscan done

--- dbscan.cpp
+++ dbscan.cpp
@@ -34,6 +34,8 @@
 #define pb push_back
 
 const int INF = 1e9;
+const int MAX_RAND = 1e9;
+const double DERIVATIVE = 0.5;
 
 using namespace std;
 //почему нельзя задать размер вектора в классе
@@ -43,8 +45,8 @@ class Shell;
 class Point;
 class SetPoints;
 bool comp(Point a, Point b);
-int dist(Point a, Point b);
-int vect_comp(Point a, Point b, Point p);
+double distAB(Point a, Point b);
+double vect_comp(Point a, Point b, Point p);
 
 
 mt19937 _g(time(nullptr));
@@ -56,12 +58,14 @@ int randint(int a, int b) {
 
 class Point{
 	public:
-		int x = 0, y = 0, type = 0;
-		pair<int, int> neighbors[5] = {{0, INF}, {0, INF}, 
+		int type = 0;
+		bool used = false;
+		double x, y;
+		pair<int, double> neighbors[5] = {{0, INF}, {0, INF}, 
 							{0, INF}, {0, INF}, {0, INF}};
 
 		Point() {}
-		Point(int x, int y) {
+		Point(double x, double y) {
 			this -> x = x;
 			this -> y = y;
 		}
@@ -77,12 +81,12 @@ class Point{
 			
 		}
 
-		int latest(int num) {
-			return (this -> neighbors)[num].sec;
+		double latest(int num) {
+			return (this -> neighbors)[num - 1].sec;
 		}
 
 
-		void insert_neighbor(pair<int, int> neighbor) {
+		void insert_neighbour(pair<int, double> neighbor) {
 			if ((this -> neighbors)[4].sec < neighbor.sec) return;
 			(this -> neighbors)[4] = neighbor;
 			for (int i = 4; i > 0; i--) {
@@ -97,11 +101,12 @@ class Point{
 class SetPoints{
 	public:
 		vector <Point> mas;
+		vector <double> distanses;
 		int size;
 		void scan(string file) {
 			ifstream fin;
 			fin.open(file);
-			int x, y;
+			double x, y;
 			while (fin >> x) {
 				fin >> y;
 				Point a(x, y);
@@ -116,23 +121,83 @@ class SetPoints{
 			for (int i = 0; i < mas.size(); i++) {
 				for (int j = 0; j < mas.size(); j++) {
 					if (i == j) continue;
-					int d = dist((this -> mas)[i], (this -> mas)[j]);
-					(this -> mas)[i].insert_neighbor({j, d});
+					double d = distAB((this -> mas)[i], mas[j]);
+					mas[i].insert_neighbour({j, d});
 				}
 			}
 		}
 
-		void sorting() {
+		/*void sorting() {
+			//change this place
 			sort(mas.begin(), mas.end(), comp);
-		}
+		}*/
 
 		void print() {
 			ofstream fout;
 			fout.open("dist_to_neighbour.txt");
-			for (int i = 0; i < mas.size(); i++) {
-				fout << i + 1 << " " << mas[i].latest(4) << endl;
+			
+			for (int i = 0; i < distanses.size(); i++) {
+				fout << i + 1 << " " << distanses[i] << endl;
 			}
 			fout.close();
+		}
+		void print_answer() {
+			ofstream fout;
+			fout.open("output.txt");
+			for (int i = 0; i < mas.size(); i++) {
+				fout << mas[i].x << " " << mas[i].y << " ";
+				fout << mas[i].type * 10 << "\n";
+			}
+			fout.close();
+		}
+		double find_optimal_dist() {
+			distanses.resize(mas.size());
+			for (int i = 0; i < distanses.size(); i++) {
+				distanses[i] = mas[i].latest(4);
+			}
+			sort(distanses.begin(), distanses.end());
+			int dX = distanses.size() / 50;
+			int step = distanses.size() / 100;
+			double dist = 0.0;
+			//cout << "MAS.SIZE(): " << distanses.size() << "\n";
+			for (int i = 0; i < distanses.size(); i += step) {
+				if (i + dX >= distanses.size()) {
+					dist = distanses[i];
+					return dist;
+				}
+				double dY = distanses[i + dX] - distanses[i];
+				double der = dY/dX;
+				//cout << "der: " << der << "\n";
+				if (der >= DERIVATIVE) {
+					dist = distanses[i];
+					return dist;
+				}
+			}
+			return dist;
+		}
+		void connect(double dist) {
+			int CLASTER_NUM = 1;
+			for (int i = 0; i < mas.size(); i++) {
+				if (mas[i].used) continue;
+				mas[i].used = true;
+				if (mas[i].latest(4) <= dist) { // основная точка
+					dfs(i, CLASTER_NUM, dist);
+					CLASTER_NUM++;
+				}
+			}
+		}
+		void dfs(int num, int col, double dist) {
+			mas[num].used = true;
+			if (mas[num].latest(4) > dist) {
+				return;
+			}
+			mas[num].type = col;
+			for (int i = 0; i < mas.size(); i++) {
+				if (mas[i].used) continue;
+				if (distAB(mas[num], mas[i]) <= dist) {
+					dfs(i, col, dist);
+				}
+			}
 		}
 
 };
@@ -145,7 +210,7 @@ class Shell{
 		void get(string file) {
 			ifstream fin;
 			fin.open(file);
-			int x, y;
+			double x, y;
 			while (fin >> x, x != -1) {
 				fin >> y;
 				Point p(x, y);
@@ -158,13 +223,14 @@ class Shell{
 				i.show();
 			}
 		}
-		void fill(int min_x, int max_x, int min_y, int max_y, int color) {
+		void fill(double min_x, double max_x, double min_y, double max_y, int color) {
 			ofstream fout;
 			fout.open("data_dbscan.txt", ios::app);
 			
-			for (int i = 0, x, y; i < 5000; i++) {
-				x = randint(min_x, max_x);
-				y = randint(min_y, max_y);
+			for (int i = 0; i < 5000; i++) {
+				double x, y;
+				x = min_x + (max_x - min_x) * (double)(rand() % RAND_MAX) / RAND_MAX;
+				y = min_y + (max_y - min_y) * (double)(rand() % RAND_MAX) / RAND_MAX;
 				Point l, r, a(x, y);
 				int up = 0;
 				for (int i = 0; i < points.size(); i++) {
@@ -219,8 +285,8 @@ class Menu{
 					cin >> num;
 					int color = 20;
 					for (int i = 0; i < num; i++) {
-						fout << randint(1, 500) << " ";
-						fout << randint(1, 500) << " ";
+						fout << (double) (rand() % 500 + rand() / MAX_RAND) << " ";
+						fout << (double) (rand() % 500 + rand() / MAX_RAND) << " ";
 						fout << color << "\n";
 					}
 					fout.close();
@@ -239,8 +305,10 @@ class Menu{
 					SetPoints s;
 					s.scan(file);
 					s.fill_neighbors();
-					s.sorting();
+					double dist = s.find_optimal_dist();
+					s.connect(dist);
 					s.print();
+					s.print_answer();
 				} else if (t == '9') {
 					break;
 				}
@@ -253,7 +321,10 @@ class Menu{
 			string output = "data.txt";
 			ofstream fout;
 			fout.open(output);
-			int x, y, color;
+			fout.setf(ios::fixed);
+			fout.precision(7);
+			int color;
+			double x, y;
 			while (fin >> x) {
 				fin >> y >> color;
 				fout << x << " " << y << "\n";
@@ -268,16 +339,19 @@ bool comp(Point a, Point b) {
 	return a.latest(4) < b.latest(4);
 }
 
-int dist(Point a, Point b) {
-	return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+double distAB(Point a, Point b) {
+	return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 }
 
-int vect_comp(Point a, Point b, Point p) {
+double vect_comp(Point a, Point b, Point p) {
 	return (a.x - p.x) * (b.y - p.y) - (a.y - p.y) * (b.x - p.x);
 }
 
 
 int main() {
+	cout.setf(ios::fixed);
+	cout.precision(7);
+	srand((unsigned)time(NULL));
 	Menu men1;
 	men1.start();
 	return 0;
